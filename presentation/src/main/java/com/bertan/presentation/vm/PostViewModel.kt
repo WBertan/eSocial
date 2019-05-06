64f305df90b8b2557bdcadfe1d051e3d2f04ea86
WBertan <william.bertan@gmail.com>
2019-05-06 19:45:14 +0100

    Add view model responsible to get and add a post.

---
+++ presentation/src/main/java/com/bertan/presentation/vm/PostViewModel.kt
@@ -0,0 +1,32 @@
+package com.bertan.presentation.vm
+
+import com.bertan.domain.interactor.post.AddPost
+import com.bertan.domain.interactor.post.GetPost
+import com.bertan.presentation.mapper.PostMapper.asPostView
+import com.bertan.presentation.mapper.PostViewMapper.asPost
+import com.bertan.presentation.model.PostView
+import java.util.*
+
+class PostViewModel(private val getPostUseCase: GetPost, private val addPostUseCase: AddPost) :
+    RxUseCaseViewModel<Optional<PostView>>(getPostUseCase, addPostUseCase) {
+    override fun onCreateViewModel() = Unit
+
+    fun getPost(postId: String) {
+        postLoading()
+
+        getPostUseCase.execute(
+            onNext = { post -> post.map { it.asPostView }.postSuccess() },
+            onError = { it.postError("Failed to load Post($postId)!") }
+        )
+    }
+
+    fun addPost(post: PostView) {
+        postLoading()
+
+        addPostUseCase.execute(
+            AddPost.Param(post.asPost),
+            onComplete = { Optional.of(post).postSuccess() },
+            onError = { it.postError("Failed to add $post!") }
+        )
+    }
+}
---
+++ presentation/src/test/java/com/bertan/presentation/vm/PostViewModelSpec.kt
@@ -0,0 +1,127 @@
+package com.bertan.presentation.vm
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
+import com.bertan.domain.interactor.post.AddPost
+import com.bertan.domain.interactor.post.GetPost
+import com.bertan.domain.model.Post
+import com.bertan.presentation.mapper.PostMapper.asPostView
+import com.bertan.presentation.state.ViewState
+import com.bertan.presentation.test.PostDataFactory
+import com.bertan.presentation.test.PostViewDataFactory
+import io.mockk.*
+import io.mockk.impl.annotations.MockK
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import java.util.*
+
+class PostViewModelSpec {
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var postViewModel: PostViewModel
+
+    @MockK(relaxed = true)
+    private lateinit var getPostUseCase: GetPost
+    @MockK(relaxed = true)
+    private lateinit var addPostUseCase: AddPost
+    @MockK
+    private lateinit var lifecycleOwner: LifecycleOwner
+
+    private lateinit var lifecycle: LifecycleRegistry
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+        postViewModel = PostViewModel(getPostUseCase, addPostUseCase)
+
+        lifecycle = LifecycleRegistry(lifecycleOwner)
+        lifecycle.addObserver(postViewModel)
+    }
+
+    @Test
+    fun `given a lifecycle when ON_CREATE it should not interact with the use cases`() {
+        lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
+
+        confirmVerified(getPostUseCase)
+        confirmVerified(addPostUseCase)
+    }
+
+    @Test
+    fun `given an postId when call getPost it should post loading state`() {
+        postViewModel.getPost("postId")
+
+        assertEquals(ViewState.Loading, postViewModel.getState().value)
+    }
+
+    @Test
+    fun `given a success use case when executes getPost it should post success state`() {
+        val post = PostDataFactory.get()
+        every { getPostUseCase.execute(any(), captureLambda(), any(), any()) } answers {
+            lambda<Function1<Optional<Post>, Unit>>().invoke(Optional.of(post))
+            mockk()
+        }
+
+        postViewModel.getPost("postId")
+
+        assertEquals(ViewState.Success(Optional.of(post.asPostView)), postViewModel.getState().value)
+    }
+
+    @Test
+    fun `given a failure use case when executes getPost it should post error state`() {
+        val dummyError = Exception("dummyError")
+        every { getPostUseCase.execute(any(), any(), captureLambda(), any()) } answers {
+            lambda<Function1<Throwable, Unit>>().invoke(dummyError)
+            mockk()
+        }
+
+        postViewModel.getPost("postId")
+
+        assertEquals(
+            ViewState.Error("Failed to load Post(postId)!", dummyError),
+            postViewModel.getState().value
+        )
+    }
+
+    @Test
+    fun `given an post when call addPost it should post loading state`() {
+        val postView = PostViewDataFactory.get()
+        postViewModel.addPost(postView)
+
+        assertEquals(ViewState.Loading, postViewModel.getState().value)
+    }
+
+    @Test
+    fun `given a success use case when executes addPost it should post success state`() {
+        val postView = PostViewDataFactory.get()
+        every { addPostUseCase.execute(any(), captureLambda(), any()) } answers {
+            lambda<Function0<Unit>>().invoke()
+            mockk()
+        }
+
+        postViewModel.addPost(postView)
+
+        assertEquals(ViewState.Success(Optional.of(postView)), postViewModel.getState().value)
+    }
+
+    @Test
+    fun `given a failure use case when executes addPost it should post error state`() {
+        val postView = PostViewDataFactory.get()
+        val dummyError = Exception("dummyError")
+        every { addPostUseCase.execute(any(), any(), captureLambda()) } answers {
+            lambda<Function1<Throwable, Unit>>().invoke(dummyError)
+            mockk()
+        }
+
+        postViewModel.addPost(postView)
+
+        assertEquals(
+            ViewState.Error("Failed to add ${postView}!", dummyError),
+            postViewModel.getState().value
+        )
+    }
+}
